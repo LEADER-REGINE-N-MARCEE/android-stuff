b812b9cddcb74c7c28a5aaf9fc64cddb7cbdade0
Marcy Bunag <marcybunag@gmail.com>
2021-01-07 15:50:14 +0800

    .
    .

--- app/src/main/java/com/example/mobileactivity/MainActivity.java
+++ app/src/main/java/com/example/mobileactivity/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.mobileactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnsignin = (Button) findViewById(R.id.btnsignin);
+        btnsignin.setOnClickListener(this);
+
+        Button btnregister = (Button) findViewById(R.id.btnregister);
+        btnregister.setOnClickListener(this);
+
+        Button btnforgot = (Button) findViewById(R.id.btnforgot);
+        btnforgot.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnsignin:
+                Intent screen2 = new Intent(this, MainActivity2.class);
+                startActivity(screen2);
+                break;
+            case R.id.btnregister:
+                Toast.makeText(this, "Not Yet Available", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnforgot:
+                Toast.makeText(this, "Not Yet Available", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/mobileactivity/MainActivity2.java
+++ app/src/main/java/com/example/mobileactivity/MainActivity2.java
@@ -2,13 +2,41 @@ package com.example.mobileactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+
+    EditText textaccountnumber;
+    EditText textpassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textaccountnumber = (EditText) findViewById(R.id.textaccountnumber);
+        textpassword = (EditText) findViewById(R.id.textpassword);
+
+
+        Button btnlogin = (Button) findViewById(R.id.btnlogin);
+        btnlogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (textaccountnumber.getText().toString().equals("123456") && textpassword.getText().toString().equals("password")) {
+            Toast.makeText(this, "Login Succesful", Toast.LENGTH_SHORT).show();
+            Intent screen3 = new Intent(this, MainActivity3.class);
+            startActivity(screen3);
+        }
+       else {
+            Toast.makeText(this, "Incorrect Account Number and Password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
